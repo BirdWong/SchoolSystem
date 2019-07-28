e7e9ffb844860474472fdbb1f29f0c3dd3bb47aa
RocWong <912719483@qq.com>
2019-07-28 20:58:09 +0800

    补充实体文档

--- gateway_4795/src/main/java/cn/jsuacm/gateway/pojo/Authentication.java
+++ gateway_4795/src/main/java/cn/jsuacm/gateway/pojo/Authentication.java
@@ -1,7 +1,11 @@
 package cn.jsuacm.gateway.pojo;
 
 import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -13,14 +17,20 @@ import java.io.Serializable;
  */
 @TableName(value = "authentication")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "用户权限对象")
 public class Authentication implements Serializable{
 
     @TableId(value = "aid", type = IdType.AUTO)
+    @ApiModelProperty(value = "用户权限对象id", dataType = "int")
     private int aid;
 
     @TableField(value = "uid", strategy = FieldStrategy.NOT_NULL)
+    @ApiModelProperty(value = "用户的id", required = true, dataType = "int")
     private int uid;
 
     @TableField(value = "role", strategy = FieldStrategy.NOT_NULL)
+    @ApiModelProperty(value = "权限名称", dataType = "string", required = true)
     private String role;
 }
--- gateway_4795/src/main/java/cn/jsuacm/gateway/pojo/User.java
+++ gateway_4795/src/main/java/cn/jsuacm/gateway/pojo/User.java
@@ -1,7 +1,11 @@
 package cn.jsuacm.gateway.pojo;
 
 import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -13,17 +17,22 @@ import java.io.Serializable;
  */
 @TableName(value = "user")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "用户对象")
 public class User implements Serializable{
 
     /**
      * 用户id，自增长
      */
     @TableId(value = "uid", type = IdType.AUTO)
+    @ApiModelProperty(value = "用户的id", dataType = "int")
     private int uid;
 
     /**
      * 用户账号，可用于登录（学号为主）
      */
+    @ApiModelProperty(value = "用户的账号，以学号为主，注册后不可修改", required = true, dataType = "string")
     @TableField(strategy = FieldStrategy.NOT_NULL,value = "account_number")
     private String accountNumbser;
 
@@ -31,24 +40,28 @@ public class User implements Serializable{
     /**
      * 用户邮箱
      */
+    @ApiModelProperty(value = "用户绑定的邮箱", required = true, dataType = "string")
     @TableField(strategy = FieldStrategy.NOT_NULL, value = "email")
     private String email;
 
     /**
      * 用户昵称
      */
+    @ApiModelProperty(value = "用户的昵称", required = true, dataType = "string")
     @TableField(strategy = FieldStrategy.NOT_NULL, value = "username")
     private String username;
 
     /**
      * 用户密码
      */
+    @ApiModelProperty(value = "用户的密码", required = true, dataType = "string")
     @TableField(strategy = FieldStrategy.NOT_NULL, value = "password")
     private String password;
 
     /**
      * 头像url
      */
+    @ApiModelProperty(value = "用户的头像, 如果用户没有设置， 也就是查询出来的信息这个时空，请直接前端直接指向一个头像", required = false, dataType = "string")
     @TableField(value = "picture_url")
     private String prictureUrl;
 
